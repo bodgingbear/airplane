b032e4faf8f42bfebb579d25bf0b4e3fa421c4f2
OfftheCode <offthecode@gmail.com>
2020-11-01 08:47:28 +0100

    Add timer

--- src/scenes/UIScene.ts
+++ src/scenes/UIScene.ts
@@ -3,6 +3,8 @@ import { AltitudeProvider } from "objects/altitudeProvider";
 export class UIScene extends Phaser.Scene {
    alitudeProvider!: AltitudeProvider
     text!: Phaser.GameObjects.Text
+    timer!: Phaser.GameObjects.Text
+
   public constructor() {
     super({
       key: 'UIScene',
@@ -13,12 +15,41 @@ export class UIScene extends Phaser.Scene {
       return `ALTITUDE ${this.alitudeProvider.altitude.toString()}`
   }
 
+  timeValue: number = 0
+
   init({altitudeProvider}: {altitudeProvider: AltitudeProvider}) {
       this.alitudeProvider = altitudeProvider
   }
 
   public create(): void {
-  this.text= this.add.text(25, 25, this.description).setFontSize(40)
+  this.text= this.add.text(25, 25, this.description).setFontSize(40);
+  this.timer = this.add.text(25, 85, `Time: ${this.formatTime(0)}`)
+  .setFontSize(40);
+
+  this.time.addEvent({
+      delay: 100,
+      loop: true,
+      callback: this.updateTimer
+  })
+
+  }
+
+  updateTimer = () => {
+    this.timer.setText(this.formatTime(this.timeValue+=100))
+  }
+
+  formatTime = (time: number): string => {
+      console.log(time);
+      const seconds = Math.floor(time / 1000);
+      if (seconds < 1) {
+          return "00:00"
+      } else if (seconds >= 1 && seconds < 60) {
+        return seconds > 9 ? `00:${seconds}` : `00:0${seconds}`
+      } else {
+          let minutes = Math.floor(seconds / 60);
+          let leftSeconds = seconds % 60 > 9 ? `${Math.floor(seconds % 60)}` : `0${Math.floor(seconds % 60)}`
+          return `0${minutes}:${leftSeconds}`
+      }
   }
 
   public update(): void {
