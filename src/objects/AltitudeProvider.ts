9e0889c59bbdc2b63e55e332a9b58a5753f0d0fe
bibixx <bartosz@legiec.io>
2020-11-01 10:02:17 +0100

    Add intro

--- src/objects/AltitudeProvider.ts
+++ src/objects/AltitudeProvider.ts
@@ -1,39 +1,39 @@
 import { EventEmitter } from 'packages/utils';
 import { SCREEN_HEIGHT, SCREEN_WIDTH, ZOOM } from '../constants';
 
-import { Obstacle } from '../objects/Obstacle';
-
+import { Obstacle } from './Obstacle';
 
 export class AltitudeProvider extends EventEmitter<'plane-crashed'> {
+  altitude: number;
 
-    altitude: number 
+  constructor(
+    private scene: Phaser.Scene,
+    private obstacles: Obstacle[],
+    private initialAltitude: number = 12000
+  ) {
+    super();
+    this.altitude = initialAltitude;
 
-    constructor(
-        private scene: Phaser.Scene,
-        private obstacles: Obstacle[],
-        private initialAltitude: number = 12000
-        ) { 
-        super();
-        this.altitude = initialAltitude;
+    scene.time.addEvent({
+      delay: 120,
+      loop: true,
+      callback: this.checkObstacles,
+    });
+  }
 
-        scene.time.addEvent({
-            delay: 120,
-            loop: true,
-            callback: this.checkObstacles
-        })
+  checkObstacles = () => {
+    if (this.altitude <= 0) {
+      this.emit('plane-crashed');
+      return;
     }
 
-    checkObstacles = () => {
-        if (this.altitude <= 0 ) {
-            this.emit('plane-crashed');
-            return
-        }
-
-        this.altitude -= this.obstacles.filter((obstacle) => obstacle.functioningState === 'broken').map((obstacle) => {
-            return obstacle.altitudeDecrease
-        }).reduce( (sum, current) => {
-            return sum + current
-        }, 0);
-    }
-
+    this.altitude -= this.obstacles
+      .filter((obstacle) => obstacle.functioningState === 'broken')
+      .map((obstacle) => {
+        return obstacle.altitudeDecrease;
+      })
+      .reduce((sum, current) => {
+        return sum + current;
+      }, 0);
+  };
 }
--- src/scenes/UIScene.ts
+++ src/scenes/UIScene.ts
@@ -1,9 +1,11 @@
-import { AltitudeProvider } from "objects/altitudeProvider";
+import { AltitudeProvider } from 'objects/altitudeProvider';
 
 export class UIScene extends Phaser.Scene {
-   alitudeProvider!: AltitudeProvider
-    text!: Phaser.GameObjects.Text
-    timer!: Phaser.GameObjects.Text
+  alitudeProvider!: AltitudeProvider;
+
+  text!: Phaser.GameObjects.Text;
+
+  timer!: Phaser.GameObjects.Text;
 
   public constructor() {
     super({
@@ -11,48 +13,55 @@ export class UIScene extends Phaser.Scene {
     });
   }
 
-  get description(): string { 
-      return `ALTITUDE ${this.alitudeProvider.altitude.toString()}`
+  get description(): string {
+    return `ALTITUDE:  ${this.alitudeProvider.altitude.toString()}`;
   }
 
-  timeValue: number = 0
+  timeValue: number = 0;
 
-  init({altitudeProvider}: {altitudeProvider: AltitudeProvider}) {
-      this.alitudeProvider = altitudeProvider
+  init({ altitudeProvider }: { altitudeProvider: AltitudeProvider }) {
+    this.alitudeProvider = altitudeProvider;
   }
 
   public create(): void {
-  this.text= this.add.text(25, 25, this.description).setFontSize(40);
-  this.timer = this.add.text(25, 85, `Time: ${this.formatTime(0)}`)
-  .setFontSize(40);
+    this.text = this.add.text(25, 25, this.description, {
+      fontFamily: 'Pixel miners',
+      fontSize: '40px',
+    });
+    this.timer = this.add.text(25, 85, `Time: ${this.formatTime(0)}`, {
+      fontFamily: 'Pixel miners',
+      fontSize: '40px',
+    });
 
-  this.time.addEvent({
+    this.time.addEvent({
       delay: 100,
       loop: true,
-      callback: this.updateTimer
-  })
-
+      callback: this.updateTimer,
+    });
   }
 
   updateTimer = () => {
-    this.timer.setText(this.formatTime(this.timeValue+=100))
-  }
+    this.timer.setText(this.formatTime((this.timeValue += 100)));
+  };
 
   formatTime = (time: number): string => {
-      console.log(time);
-      const seconds = Math.floor(time / 1000);
-      if (seconds < 1) {
-          return "00:00"
-      } else if (seconds >= 1 && seconds < 60) {
-        return seconds > 9 ? `00:${seconds}` : `00:0${seconds}`
-      } else {
-          let minutes = Math.floor(seconds / 60);
-          let leftSeconds = seconds % 60 > 9 ? `${Math.floor(seconds % 60)}` : `0${Math.floor(seconds % 60)}`
-          return `0${minutes}:${leftSeconds}`
-      }
-  }
+    console.log(time);
+    const seconds = Math.floor(time / 1000);
+    if (seconds < 1) {
+      return '00:00';
+    }
+    if (seconds >= 1 && seconds < 60) {
+      return seconds > 9 ? `00:${seconds}` : `00:0${seconds}`;
+    }
+    const minutes = Math.floor(seconds / 60);
+    const leftSeconds =
+      seconds % 60 > 9
+        ? `${Math.floor(seconds % 60)}`
+        : `0${Math.floor(seconds % 60)}`;
+    return `0${minutes}:${leftSeconds}`;
+  };
 
   public update(): void {
-    this.text.setText(this.description)
+    this.text.setText(this.description);
   }
 }
